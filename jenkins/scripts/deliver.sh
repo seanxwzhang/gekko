45b2cecd6eadbe8ed006de6c19da0f5aa73e3a36
Xiaowen Zhang <seanxwzhang@gmail.com>
2018-01-12 17:06:56 -0800

    added build.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 # This script bootstraps Gekko using pm2
 
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-cd $SCRIPTPATH/../..
-echo $(pwd)
-echo $(ls)
-pm2 stop gekko
-pm2 start gekko.js -- -ui
+pm2 kill
+pm2 start gekko.js -- --ui
